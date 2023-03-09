c59b0a49934cfac6a461822cd60b0e3f55d612b6
jz <jz@qq.com>
2023-03-09 21:39:03 +0800

    first commit

---
+++ src/main/java/com/atguigu/redis_soringboot/RedisSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.redis_soringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/atguigu/redis_soringboot/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.redis_soringboot.git;
+
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+
+
+
+}
